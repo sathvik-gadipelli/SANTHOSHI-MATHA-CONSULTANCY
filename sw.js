3653058ab37789bc8ae73ee987421e35506ddf7b
sathvik-gadipelli <company.rosh07@gmail.com>
2026-04-14 21:14:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME = "smc-v5";
+const CACHE_NAME = "smc-v6";
 
 const BASE = "/SANTHOSHI-MATHA-CONSULTANCY/";
 
@@ -9,29 +8,26 @@ const urlsToCache = [
   BASE + "manifest.json",
   BASE + "icon-192.png",
   BASE + "icon-512.png"
-  
-] ;
+];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => k !== CACHE_NAME ? caches.delete(k) : null))
     )
   );
   self.clients.claim();
-  
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
-  
 });
